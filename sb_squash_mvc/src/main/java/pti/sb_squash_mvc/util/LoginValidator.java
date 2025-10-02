29c019af4561240474c5748701c9b79138024395
succesminded <succesminded@gmail.com>
2025-10-02 20:35:48 +0200

    util.LoginValidator.isUserLoggedIn metódus váza és util.Roles enum

--- sb_squash_mvc/src/main/java/pti/sb_squash_mvc/util/LoginValidator.java
+++ sb_squash_mvc/src/main/java/pti/sb_squash_mvc/util/LoginValidator.java
@@ -1,5 +1,18 @@
 package pti.sb_squash_mvc.util;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class LoginValidator {
 
+	public Roles isUserLoggedIn(int userId)
+	{
+		Roles result = Roles.UNKNOWN;
+		
+		
+		
+		return result;
+		
+	}
+	
 }
---
+++ sb_squash_mvc/src/main/java/pti/sb_squash_mvc/util/Roles.java
@@ -0,0 +1,7 @@
+package pti.sb_squash_mvc.util;
+
+public enum Roles {
+	
+	ADMIN, PLAYER, UNKNOWN
+
+}
