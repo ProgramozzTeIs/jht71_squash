0fc94ed38fb7c77e56abe5ec5bd8ecb6f2bf6525
Pherlita <pherlita@gmail.com>
2025-10-03 07:32:17 +0200

    add dto package, userDto, errorDto

---
+++ sb_squash_mvc/src/main/java/pti/sb_squash_mvc/dto/ErrorDto.java
@@ -0,0 +1,22 @@
+package pti.sb_squash_mvc.dto;
+
+public class ErrorDto {
+	
+	private int code;
+
+	public ErrorDto(int code) {
+		super();
+		this.code = code;
+	}
+
+	public int getCode() {
+		return code;
+	}
+
+	public void setCode(int code) {
+		this.code = code;
+	}
+	
+	
+
+}
---
+++ sb_squash_mvc/src/main/java/pti/sb_squash_mvc/dto/UserDto.java
@@ -0,0 +1,32 @@
+package pti.sb_squash_mvc.dto;
+
+public class UserDto {
+	
+	private int id;
+	private String name;
+	
+	public UserDto(int id, String name) {
+		super();
+		this.id = id;
+		this.name = name;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+
+}
