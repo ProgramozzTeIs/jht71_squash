b1af40d9d6b11dd858d1f1d6354099cf54d44e1f
Pandrea7 <pergeandrea@gmail.com>
2025-10-04 10:42:33 +0200

    AdminDto

---
+++ sb_squash_mvc/src/main/java/pti/sb_squash_mvc/dto/AdminDto.java
@@ -0,0 +1,53 @@
+package pti.sb_squash_mvc.dto;
+
+import java.util.Set;
+
+public class AdminDto {
+
+	private Set<UserDto> users;
+	private Set<PalceDto> places;
+	private int loggedInAdminId;
+	private int code;
+	
+	
+	public AdminDto(Set<UserDto> users, Set<PalceDto> places, int loggedInAdminId, int code) {
+		super();
+		this.users = users;
+		this.places = places;
+		this.loggedInAdminId = loggedInAdminId;
+		this.code = code;
+	}
+	
+	
+	public Set<UserDto> getUsers() {
+		return users;
+	}
+	
+	public void setUsers(Set<UserDto> users) {
+		this.users = users;
+	}
+	public Set<PalceDto> getPlaces() {
+		return places;
+	}
+	
+	public void setPlaces(Set<PalceDto> places) {
+		this.places = places;
+	}
+	
+	public int getLoggedInAdminId() {
+		return loggedInAdminId;
+	}
+	
+	public void setLoggedInAdminId(int loggedInAdminId) {
+		this.loggedInAdminId = loggedInAdminId;
+	}
+	
+	public int getCode() {
+		return code;
+	}
+	
+	public void setCode(int code) {
+		this.code = code;
+	}
+	
+}
