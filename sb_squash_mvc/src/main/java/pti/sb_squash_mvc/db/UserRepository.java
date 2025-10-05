8aa0f5a50056d1261ac0ceb9aac1ac163d7cecba
succesminded <succesminded@gmail.com>
2025-10-05 09:29:36 +0200

    userRepo optional import nem kell

--- sb_squash_mvc/src/main/java/pti/sb_squash_mvc/db/UserRepository.java
+++ sb_squash_mvc/src/main/java/pti/sb_squash_mvc/db/UserRepository.java
@@ -1,7 +1,5 @@
 package pti.sb_squash_mvc.db;
 
-import java.util.Optional;
-
 import org.springframework.data.jdbc.repository.query.Modifying;
 import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.repository.CrudRepository;
